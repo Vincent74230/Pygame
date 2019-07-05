a7b41e0dcd4f634465a9e1aa5bc25a5ca4c466c5
Vincent74230 <vincent.nowak@hotmail.fr>
2019-07-05 14:25:26 +0200

    en pleine reflexion pour faire bouger mac gyver..

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 import sys
 import pygame
 import settings
-
-
+x = 0
+y = 0
+x_change = 0
+x += x_change
 
 class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode(settings.SCREEN_SIZE)
         self.macgyver = pygame.image.load("MacGyver.png")
-        #self.rect = self.macgyver.get_rect()
-        self.screen.blit(self.macgyver,(0,0))
+        pygame.display.update()
+
+        
+    def mac(self,x,y):
+        self.screen.blit(self.macgyver,(x,y))
         pygame.display.update()
 
     def start(self):
         while 1:  #main loop of the game
             self.quit()
             self.moves()
+            self.mac()
 
     def quit(self):  #fonction which manages the quit button
         for event in pygame.event.get(pygame.QUIT):
@@ -32,6 +38,14 @@ class Game:
                 print("On va a gauche")
             else:
                 print("On va a droite")
+                x_change = 5
+        for event in pygame.event.get(pygame.KEYUP):
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                x_change = 0
+        
+        pygame.display.update()
+
+
 
 def main():
     game = Game()
