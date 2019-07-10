6b0fc89e767f83e69100d31f77728922933f62a7
Vincent74230 <vincent.nowak@hotmail.fr>
2019-07-10 10:21:29 +0200

    MacGYver bouge et évite les murs et le décor est posé

--- clas.py
+++ clas.py
@@ -23,3 +23,39 @@ class Level:
                     screen.blit(bones,(x,y))
                 square_number += 1
             line_number += 1
+
+
+class MacGyver:
+    def __init__(self):
+        self.mac = pygame.image.load(settings.MACIMG).convert_alpha()
+        self.level = settings.LEVEL
+        self.sprite_x = 0
+        self.sprite_y = 0
+        self.x = 0
+        self.y = 0
+        
+
+    def moves(self,move):
+        if move == 'right':
+            if self.sprite_x<14:
+                if self.level[self.sprite_y][self.sprite_x+1] != 1:
+                    self.sprite_x +=1
+                    self.x = self.sprite_x * settings.SPRITE_SIZE
+
+        if move == 'left':
+            if self.sprite_x>0:
+                if self.level[self.sprite_y][self.sprite_x-1] != 1:
+                    self.sprite_x -=1
+                    self.x = self.sprite_x * settings.SPRITE_SIZE
+
+        if move == 'up':
+            if self.sprite_y>0:
+                if self.level[self.sprite_y-1][self.sprite_x] != 1:
+                    self.sprite_y -= 1
+                    self.y = self.sprite_y * settings.SPRITE_SIZE
+
+        if move == 'down':
+            if self.sprite_y < 14:
+                if self.level[self.sprite_y+1][self.sprite_x] !=1:
+                    self.sprite_y +=1
+                    self.y = self.sprite_y * settings.SPRITE_SIZE
--- essaimac.py
+++ essaimac.py
@@ -9,16 +9,35 @@ screen = pygame.display.set_mode(settings.SCREEN_SIZE)
 background = pygame.image.load(settings.BACKGROUND).convert()
 
 l = clas.Level()
+mac = clas.MacGyver()
+"""
 screen.blit(background,(0,0))
 l.disp_level(screen)
-
+mac.moves(screen)
+"""
 pygame.display.update()
 
 launched = True
 while launched:
+    pygame.time.Clock().tick(30)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             launched = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                mac.moves('right')
+            if event.key == pygame.K_LEFT:
+                mac.moves ('left')
+            if event.key == pygame.K_UP:
+                mac.moves('up')
+            if event.key == pygame.K_DOWN:
+                mac.moves ('down')
+
+    screen.blit(background,(0,0))
+    l.disp_level(screen)
+    screen.blit(mac.mac,(mac.x,mac.y))
+    pygame.display.update()
+
 
 pygame.quit()
 
